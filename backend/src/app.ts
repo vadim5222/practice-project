d61c7b4092449853086bb1a3584258c2673f92be
Vadim Kuznetsov <vadimkuznetsov1132@yandex.ru>
2026-06-12 17:27:57 +0300

    replace frontend url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,7 +20,7 @@ class App {
         this.app.use(cookieParser())
         this.app.use(cors({
             origin: [
-                'http://localhost:3000',
+                'http://localhost:5173',
             ],
             methods: ['GET', 'POST', 'DELETE'],
             credentials: true
